c75b2256f3d0184a0d10ba50a606a3165c767199
koichiba <101hundredone@gmail.com>
2020-02-02 16:43:25 +0900

    googleログイン機能を実装・ginoreに.envを追加

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import GoogleLogin, { GoogleLogout } from "react-google-login";
 
 const Login = () => {
-  return <div>Hello</div>;
+  const responseGoogle1 = (response?: any) => {
+    console.log(response);
+    console.log("成功");
+  };
+  const responseGoogle2 = (response?: any) => {
+    console.log(response);
+    console.log("失敗");
+  };
+  if (!process.env.REACT_APP_GOOGLE_CLIENT_ID) return null;
+  const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+  return (
+    <>
+      <GoogleLogin
+        clientId={clientId}
+        buttonText="ログイン"
+        onSuccess={responseGoogle1}
+        onFailure={responseGoogle2}
+        cookiePolicy={"single_host_origin"}
+      />
+      <div>Hello</div>
+      <GoogleLogout
+        clientId={clientId}
+        buttonText="Logout"
+        onLogoutSuccess={responseGoogle1}
+      ></GoogleLogout>
+    </>
+  );
 };
 
 export default Login;
